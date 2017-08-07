d49e39a4629e12c434d7cbe768310c67bf9efc0c
NilGE <gxy26904@gmail.com>
2017-08-06 21:37:49 -0400

    seperate dev and deploy

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const config = require('./config/config.js');
+
+
+const app = express();
+
+// Setup view engine
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.resolve(path.join(__dirname, '/dist'))));
+
+app.get('*', (req, res) => {
+  res.render('index');
+});
+
+app.listen(config.port, config.host, () => {
+  console.info('Blog running at port ', config.port);
+});
